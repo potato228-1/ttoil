21887d053e945e0d4121282f3a9572a5db561624
Potato228 <petruhakabachok@gmail.com>
2025-01-14 22:54:35 +0800

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
 	output: "export",
-	basePath: "/ttoil",
-	assetPrefix: "/ttoil",
 };
 
-module.exports = nextConfig;
 export default nextConfig;
