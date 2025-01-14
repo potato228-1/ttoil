089f2793e1397578d14994aa24a2d0373315b233
Potato228 <petruhakabachok@gmail.com>
2025-01-14 18:55:04 +0800

    images fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  basePath: "/ttoil"
+	/* config options here */
+	output: "export",
+	basePath: "/ttoil",
+	assetPrefix: "/ttoil",
 };
 
 export default nextConfig;
